225daa320a6cc172cb80a496241b0f9c34d5d074
Marek Stój <marek.stoj@gmail.com>
2012-03-05 18:57:29 +0100

    Added UberDeployer.Database project.

---
+++ Src/UberDeployer.Database/Schema/1.0.sql
@@ -0,0 +1,15 @@
+﻿USE [UberDeployer]
+GO
+
+CREATE TABLE [dbo].[DeploymentRequest] (
+  [Id] [int] IDENTITY(1,1) NOT NULL,
+  [DateRequested] [datetime] NOT NULL,
+  [RequesterIdentity] [nvarchar](256) NOT NULL,
+  [ProjectName] [nvarchar](256) NOT NULL,
+  [TargetEnvironmentName] [nvarchar](64) NOT NULL,
+  [FinishedSuccessfully] [bit] NOT NULL,
+  CONSTRAINT [PK_DeploymentRequest] PRIMARY KEY CLUSTERED (
+    [Id] ASC
+  )
+)
+GO
