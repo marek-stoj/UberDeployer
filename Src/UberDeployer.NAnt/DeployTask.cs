b9a8b56a7ac553ab73f918b93cb543df3d3b3590
Marek Stój <marek.stoj@gmail.com>
2012-03-06 08:58:03 +0100

    Added UberDeployer.NAnt project.

---
+++ Src/UberDeployer.NAnt/DeployTask.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using NAnt.Core;
+using NAnt.Core.Attributes;
+using UberDeployer.CommonConfiguration;
+using UberDeployer.Core.Domain;
+using UberDeployer.Core.Deployment;
+using UberDeployer.Core.Deployment.Pipeline;
+
+namespace UberDeployer.NAnt
+{
+  [TaskName("Deploy")]
+  public class DeployTask : Task
+  {
+    [TaskAttribute("projectName", Required = true)]
+    [StringValidator(AllowEmpty = false)]
+    public string ProjectName { get; set; }
+
+    [TaskAttribute("configurationName", Required = true)]
+    [StringValidator(AllowEmpty = false)]
+    public string ConfigurationName { get; set; }
+
+    [TaskAttribute("buildId", Required = true)]
+    [StringValidator(AllowEmpty = false)]
+    public string BuildId { get; set; }
+
+    [TaskAttribute("environment", Required = true)]
+    [StringValidator(AllowEmpty = false)]
+    public string Environment { get; set; }
+
+    static DeployTask()
+    {
+      Bootstraper.Bootstrap();
+    }
+
+    protected override void ExecuteTask()
+    {
+      try
+      {
+        IProjectInfoRepository projectInfoRepository = ObjectFactory.Instance.CreateProjectInfoRepository();
+
+        ProjectInfo projectInfo = projectInfoRepository.GetByName(ProjectName);
+
+        DeploymentTask deploymentTask = projectInfo.CreateDeploymentTask(
+          ObjectFactory.Instance, 
+          ConfigurationName, 
+          BuildId, 
+          Environment);
+        
+        deploymentTask.DiagnosticMessagePosted +=
+          (eventSender, tmpArgs) => Log(Level.Info, tmpArgs.Message);
+        
+        IDeploymentPipeline deploymentPipeline =
+          ObjectFactory.Instance.CreateDeploymentPipeline();
+
+        deploymentPipeline.StartDeployment(deploymentTask);
+      }
+      catch (Exception exc)
+      {
+        Log(Level.Error, "Error: " + exc);
+      }
+    }
+  }
+}
